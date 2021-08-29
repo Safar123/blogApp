4ca53b948f2d49643cd82c049c1949acc5c66302
Saffar <azajsbd111@gmail.com>
2021-08-29 20:13:00 +0545

    implementing login route

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -3,5 +3,6 @@ const userRouter = express.Router();
 const authController = require('./../controller/authController');
 
 userRouter.post('/signup', authController.signUpUser);
+userRouter.post('/login', authController.logInUser);
 
 module.exports = userRouter;
