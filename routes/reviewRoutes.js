638977a6e17c809203b9937f1afacbace8a021eb
Saffar <azajsbd111@gmail.com>
2021-09-01 10:55:18 +0545

    routes for controller

---
+++ routes/reviewRoutes.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const reviewRouter = express.Router();
+const reviewController = require('./../controller/reviewController');
+const authController = require('./../controller/authController');
+
+reviewRouter.route('/').post(authController.protectRoute, reviewController.addReview);
+reviewRouter.get('/user', authController.protectRoute, reviewController.getUserReview);
+
+reviewRouter.route('/:id')
+.delete(authController.protectRoute, reviewController.removeReview)
+.patch(authController.protectRoute, reviewController.updateReview)
+
+module.exports= reviewRouter;
