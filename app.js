f973cf6566fa54c7336a7424f243b7bcd77afe52
Saffar <azajsbd111@gmail.com>
2021-09-01 10:52:52 +0545

    Adding review resources

--- app.js
+++ app.js
@@ -2,11 +2,13 @@ const express = require('express');
 const userRouter = require('./routes/userRoutes');
 const blogRouter = require('./routes/blogRoutes');
 const errorHandler = require('./middleware/errorHandler');
+const reviewRoutes = require('./routes/reviewRoutes');
 const app = express();
 
 app.use(express.json());
 app.use('/api/v1/user', userRouter);
 app.use('/api/v1/blog', blogRouter);
+app.use('/api/v1/review', reviewRoutes);
 
 app.use(errorHandler);
 module.exports= app;
